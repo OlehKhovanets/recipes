613e5c24a750266fefd00d07f9693d10f75f72be
Oleh Khovanets <o.khovanets@siteimage.com.ua>
2024-03-29 20:29:20 +0200

    Commit

--- routes/api.php
+++ routes/api.php
@@ -1,23 +1,23 @@
 <?php
 //
-//use Illuminate\Http\Request;
-//use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 //use App\Http\Controllers\Api\Support\IndexController;
 //
 ///*SUPPORT*/
 //Route::post('/support/send', [IndexController::class, 'index'])->name('support.index');
 //
 ////todo можливо буде реалізація в майбутньому
-//Route::post('/upload/image', function (Request $request) {
-//    $image = $request->file('image');
-//    $imageName = time() . '.' . $image->getClientOriginalExtension();
-//
-//    $image->move(public_path('upload/images'), $imageName);
-//
-//    return response()->json([
-//        'success' => 1,
-//        'file' => [
-//            'url' => '/upload/images/' . $imageName,
-//        ],
-//    ]);
-//});
+Route::post('/upload/image', function (Request $request) {
+    $image = $request->file('image');
+    $imageName = time() . '.' . $image->getClientOriginalExtension();
+
+    $image->move(public_path('upload/images'), $imageName);
+
+    return response()->json([
+        'success' => 1,
+        'file' => [
+            'url' => '/upload/images/' . $imageName,
+        ],
+    ]);
+});
