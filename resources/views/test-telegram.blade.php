949aacf9ef31046f311e76911cca767ddb797f29
Oleh Khovanets <ok@turis.app>
2024-09-23 11:17:37 +0300

    test telegram

--- resources/views/test-telegram.blade.php
+++ resources/views/test-telegram.blade.php
@@ -12,24 +12,25 @@
 
 <script>
     // 1. Ініціалізація Web App
-    Telegram.WebApp.ready();
+    // Telegram.WebApp.ready();
 
     // 2. Отримання даних про користувача через initData
-    const initData = Telegram.WebApp.initData || "";
-    const initDataUnsafe = Telegram.WebApp.initDataUnsafe || {};
-
-    // Виведення даних для тестування
-    console.log("Init Data:", initData);
-    console.log("Init Data Unsafe:", initDataUnsafe);
-
-    // 3. Виведення інформації про користувача на сторінці
-    document.getElementById('user-info').innerText = `Привіт, ${initDataUnsafe.user?.first_name || 'користувач'}!`;
-
-    // Якщо необхідно отримати chat_id
-    const userId = initDataUnsafe.user?.id;
-    console.log("User ID (chat_id):", userId);
-    alert(userId);
-
+    // const initData = Telegram.WebApp.initData || "";
+    // const initDataUnsafe = Telegram.WebApp.initDataUnsafe || {};
+    //
+    // // Виведення даних для тестування
+    // console.log("Init Data:", initData);
+    // console.log("Init Data Unsafe:", initDataUnsafe);
+    //
+    // // 3. Виведення інформації про користувача на сторінці
+    // document.getElementById('user-info').innerText = `Привіт, ${initDataUnsafe.user?.first_name || 'користувач'}!`;
+    //
+    // // Якщо необхідно отримати chat_id
+    // const userId = initDataUnsafe.user?.id;
+    // console.log("User ID (chat_id):", userId);
+    // alert(userId);
+    var WebApp = window.Telegram.WebApp;
+    WebApp.showAlert(`Добро пожаловать, @${WebApp.WebAppUser.username}.`);
     // Якщо потрібно відправити ці дані на сервер
     fetch('/handle-webapp-data', {
         method: 'POST',
