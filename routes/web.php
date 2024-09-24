024ff4441d5a3b00b6145a7975e1a9499c633983
Oleh Khovanets <ok@turis.app>
2024-09-24 09:26:54 +0300

    commit

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,6 @@ use App\Models\UserRecipeSortedByDay;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function (\Illuminate\Http\Request $request) {
-    dump('test');
-    dd($request->all());
     return view('test-telegram');
 });
 //Route::get('/', [\App\Http\Controllers\Web\Home\IndexController::class, 'index'])->name('index.index');
