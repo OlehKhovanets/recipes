7f69574955a7f5b92ef7826c42877f094c2d9c45
Oleh Khovanets <ok@turis.app>
2024-09-26 17:24:45 +0300

    commit

--- resources/views/test-telegram.blade.php
+++ resources/views/test-telegram.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Telegram Web App</title>
     <script src="https://telegram.org/js/telegram-web-app.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
 <h1>Привіт, користувачу!</h1>
